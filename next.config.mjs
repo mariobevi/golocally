345ee2262fe308a5dfceb38da929e8055e496ff2
Mario Bevilacqua <130646873+mariobevi@users.noreply.github.com>
2025-08-20 12:11:49 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 import createMDX from '@next/mdx';
 
-const withMDX = createMDX({ extension: /\.mdx?$/ });
+const withMDX = createMDX({
+  extension: /\.mdx?$/ // enables .mdx pages in /app
+});
 
 /** @type {import('next').NextConfig} */
 const base = {
   pageExtensions: ['ts', 'tsx', 'mdx'],
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-    ],
-  },
+      { protocol: 'https', hostname: 'images.unsplash.com' }
+    ]
+  }
 };
 
 export default withMDX(base);
